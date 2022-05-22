91bba2851d45035d5ce1097b65b326fc64a0c4bf
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-05-22 13:08:52 +0600

    added products and product

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const Product = ({product}) => {
-  const {name, img} = product;
+  const {name, img, description, price, minimum, maximum} = product;
   return (
-    <div class="card bg-base-100 shadow-xl">
+    <div class="card bg-white ">
   <figure class="px-10 pt-10">
-    <img src={img} alt="Shoes" class="rounded-xl" />
+    <img src={img} alt="tools" class="rounded-xl" />
   </figure>
-  <div class="card-body items-center text-center">
+  <div class="card-body items-center text-center text-black">
     <h2 class="card-title">{name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className=''>${price}</h2>
+    <h2 className=''>Minimum Order Quantity: {minimum}</h2>
+    <h2 className=''>Available Quantity: {maximum}</h2>
+    <p>{description}</p>
     <div class="card-actions">
       <button class="btn btn-primary">Buy Now</button>
     </div>
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from 'react';
 import Product from './Product';
 
 const Products = () => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
   useEffect( () => {
     fetch(`products.json`)
     .then(res => res.json())
     .then(data => setProducts(data))
   },[])
   return (
-    <div>
+    <div className='bg-white'>
       <h2 className='text-black text-center text-2xl font-bold'>Machinery Tools</h2>
-      <div>
-        {/* {
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+        {
           products.map(product => <Product
-          key={product.id}
+          key={product._id}
           product={product}
           ></Product>)
-        } */}
+        }
       </div>
     </div>
   );
